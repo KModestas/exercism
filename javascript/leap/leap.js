99a9beb2a4c390067e9053cd85bd34a56d90a553
Modestas <modestask97@gmail.com>
2019-11-28 12:09:40 +0000

    isLeap all tests passing

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,18 +1,2 @@
-//
-// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-// export const isLeap = year => {
-
-export const isLeap = year => {
-  // if year divisible by 4 its a leap year
-  if (year % 4 === 0) {
-    // if year is divisible by 100 return false unless it is also divisble by 400
-    if (year % 100 === 0) {
-      if (year % 400 === 0) return true;
-      else return false;
-    }
-    // if year is not divisible by 4 - not a leap year
-  } else return false;
-};
+export const isLeap = year =>
+  (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
