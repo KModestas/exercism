4005d10b8d986210176a43e0cd467e779983b3e5
Modestas <modestask97@gmail.com>
2019-12-03 10:52:42 +0000

    progress

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -4,11 +4,28 @@
 //
 
 export class Triangle {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+    this.sidesArray = [side1, side2, side3];
+  }
+
+  // checks if there are items in array that exist and are of the same value but have a different index
+  checkDuplicateSides() {
+    return this.sidesArray.filter(
+      (item, i, array) => array.indexOf(item) !== i
+    );
   }
 
   kind() {
-    throw new Error("Remove this statement and implement this function");
+    // if all sides are the same, its an equilateral triangle
+    if (this.side1 === this.side2 && this.side2 === this.side3)
+      return "equilateral";
+
+    // if atleast one other side is the same, its an isosceles triangle
+    if (this.checkDuplicateSides().length) return "isosceles";
+    // otherwise no sides are the same and its a scalene triangle
+    else return "scalene";
   }
 }
