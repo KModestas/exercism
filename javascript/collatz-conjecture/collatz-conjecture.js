460da362205fff78e02a4b1b7ed27b011bfaad9a
Modestas <modestask97@gmail.com>
2019-12-27 13:57:02 +0000

    progress

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,29 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+export const steps = n => {
+  let steps = 0;
+
+  const calc = n => {
+    if (n === 1) return;
+    if (n % 2 === 0) {
+      n / 2;
+      steps++;
+    } else {
+      n * 3 + 1;
+      steps++;
+    }
+    return n;
+  };
+
+  calc(n);
+
+  if (n !== 1) {
+    calc(n);
+  }
+
+  console.log("n ", n);
+  console.log("steps ", steps);
+
+  return steps;
 };
