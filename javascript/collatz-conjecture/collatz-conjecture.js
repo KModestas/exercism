8b89da5b4732d1db06e9a593b3937ce1c3a4e0bb
Modestas <modestask97@gmail.com>
2019-12-28 10:26:45 +0000

    most tests passing

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -6,26 +6,15 @@
 export const steps = n => {
   let steps = 0;
 
-  const calc = n => {
-    if (n === 1) return;
+  while (n !== 1) {
     if (n % 2 === 0) {
-      n / 2;
+      n /= 2;
       steps++;
     } else {
-      n * 3 + 1;
+      n = 3 * n + 1;
       steps++;
     }
-    return n;
-  };
-
-  calc(n);
-
-  if (n !== 1) {
-    calc(n);
   }
-
-  console.log("n ", n);
-  console.log("steps ", steps);
 
   return steps;
 };
