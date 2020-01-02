813ab66cb75fc68a52add6e482726b5f25b43244
Modestas <modestask97@gmail.com>
2020-01-02 13:00:38 +0000

    first test passing!

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,8 +1,28 @@
-//
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error("Remove this statement and implement this function");
+export const decodedValue = colors => {
+  // get values of colors usings colors passed in
+  const color1 = COLORS.find(e => e.color === colors[0]).value;
+  const color2 = COLORS.find(e => e.color === colors[1]).value;
+
+  // concat color values
+  const value = "" + color1 + color2;
+
+  // return value as a number
+  return Number(value);
 };
+
+export const COLORS = [
+  { color: "black", value: 0 },
+  { color: "brown", value: 1 },
+  { color: "red", value: 2 },
+  { color: "orange", value: 3 },
+  { color: "yellow", value: 4 },
+  { color: "green", value: 5 },
+  { color: "blue", value: 6 },
+  { color: "violet", value: 7 },
+  { color: "grey", value: 8 },
+  { color: "white", value: 9 }
+];
