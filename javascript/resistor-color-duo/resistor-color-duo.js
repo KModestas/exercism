9d1d46458c07c7c5c084aa772f43668d4085318c
Modestas <modestask97@gmail.com>
2020-01-03 11:40:51 +0000

    comment above the moethod itself

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,5 +1,8 @@
 import { colorCode } from "../resistor-color/resistor-color.js";
 
+// the map only returns an index for the first 2 colors (i < 2) and an "" for the rest,
+// Number() will throw NaN
+// parseInt works because it excludes anything other than number characters
 export const decodedValue = colors =>
   parseInt(
     colors
@@ -7,7 +10,3 @@ export const decodedValue = colors =>
       .toString()
       .replace(",", "")
   );
-
-// the map only returns an index for the first 2 colors (i < 2) and an "" for the rest, 
-// Number() will throw NaN
-// parseInt works because it excludes anything other than number characters
