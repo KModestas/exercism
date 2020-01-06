c0353ca742ebc65a40e76ac01a77ddc198eebbbb
Modestas <modestask97@gmail.com>
2020-01-06 09:18:55 +0000

    refactor 4

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,15 +1,9 @@
 import { colorCode } from "../resistor-color/resistor-color.js";
 
-export const decodedValue = colors => {
-  const newColors = [];
-  for (let i = 0; i < 2; i++) newColors.push(colorCode(colors[i]));
-  return Number(newColors.join(""));
-};
-
-// es6 version
-// Number(
-//   colors
-//     .map(color => colorCode(color))
-//     .slice(0, 2)
-//     .join("")
-// );
+export const decodedValue = colors =>
+  Number(
+    colors
+      .slice(0, 2)
+      .map(colorCode)
+      .join("")
+  );
